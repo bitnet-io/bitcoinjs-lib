8339dc50263aaf71eb7ad1619ce634e43995f33c
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-01-31 21:45:13 -0700

    Update networks.js

--- src/networks.js
+++ src/networks.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, '__esModule', { value: true });
 exports.testnet = exports.regtest = exports.bitcoin = void 0;
 exports.bitcoin = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bc',
+  bech32: 'bit',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4,
   },
-  pubKeyHash: 0x00,
-  scriptHash: 0x05,
-  wif: 0x80,
+  pubKeyHash: 0x19,
+  scriptHash: 0x16,
+  wif: 0x9E,
 };
 exports.regtest = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
